4d0c61c9fbc515b0ee42a55a801532da887f8d28
Usagi233 <19825820562@163.com>
2022-04-26 00:15:16 +0800

    七日杀自动登陆签到脚本

---
+++ 7日杀论坛签到.py
@@ -0,0 +1,26 @@
+from util import *
+
+userName = sys.argv[1]
+password = sys.argv[2]
+
+# @retry(stop_max_attempt_number=5)
+def main():
+    try:
+        driver = get_web_driver()
+        driver.get("https://www.7risha.com/")
+        driver.find_element_by_xpath(
+            '''//*[@id="jinsom-sidebar-username"]''').send_keys(userName)
+        driver.find_element_by_xpath(
+            '''//*[@id="jinsom-sidebar-password"]''').send_keys(password)
+        driver.find_element_by_xpath(
+            "/html/body/div[3]/div[1]/div/div[1]/div/div[1]").click()
+        driver.find_element_by_xpath("/html/body/div[3]/div[1]/div/div[1]/div/div[5]").click()
+    # except:
+    #     raise
+    finally:
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
+    
